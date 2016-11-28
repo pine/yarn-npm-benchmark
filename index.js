20bf51654b87b8e9ac74aa8ac34c61551862dd39
Pine Mizune <pinemz@gmail.com>
2016-11-29 00:31:50 +0900

    chore(repo): initial commit

---
+++ index.js
@@ -0,0 +1,129 @@
+'use strict'
+
+const path   = require('path')
+const exec   = require('child_process').exec
+
+const _           = require('lodash')
+const csvLine     = require('csv-line')
+const fs          = require('co-fs-extra')
+const globby      = require('globby')
+const log         = require('fancy-log')
+const pEachSeries = require('p-each-series')
+const pMap        = require('p-map')
+const timeSpan    = require('time-span')
+
+// env => ndenv node version
+const ENVS = {
+  'npm@2': '4.6.2',
+  'npm@3': '6.9.1',
+  'yarn ': '6.9.1',
+}
+
+const CSV_FILENAME = 'result.csv'
+
+async function execAsync(command, opts) {
+  return new Promise((resolve, reject) => {
+    const child = exec(command, opts, (err, stdout, stderr) => {
+      if (err) { reject(err) }
+      else { resolve(null, stdout, stderr) }
+    })
+
+    child.stdout.on('data', data => process.stdout.write(data))
+    child.stderr.on('data', data => process.stderr.write(data))
+  })
+}
+
+async function execByNdenv(version, command, opts) {
+  const cmd = `NDENV_VERSION=${version} ndenv exec ${command}`
+  log(cmd)
+  return execAsync(cmd, opts)
+}
+
+async function execByEnv(env, command, opts) {
+  return execByNdenv(ENVS[env], command, opts)
+}
+
+async function showSystemInfo() {
+  const envs = Object.keys(ENVS)
+  await pEachSeries(envs, async env => {
+    log(env)
+    await execByEnv(env, 'node -v')
+
+    if (~env.indexOf('npm')) {
+      await execByEnv(env, 'npm -v')
+    } else {
+      await execByEnv(env, 'yarn -V')
+    }
+  })
+}
+
+async function cleanGlobalCache(env) {
+  if (~env.indexOf('npm')) {
+    await execByEnv(env, 'npm cache clean')
+  } else {
+    await execByEnv(env, 'yarn cache clean')
+  }
+}
+
+async function cleanLocalCache(pkgRoot) {
+  await pMap([
+    'node_modules',
+    'npm-shrinkwrap.json',
+    'yar.lock',
+  ], p => fs.remove(path.join(pkgRoot, p)))
+}
+
+async function installByEnv(env, pkgRoot) {
+  if (~env.indexOf('npm')) {
+    await execByEnv(env, 'npm install', { cwd: pkgRoot })
+  } else {
+    await execByEnv(env, 'yarn', { cwd: pkgRoot })
+  }
+}
+
+async function cleanResultCsv() {
+  await fs.remove(CSV_FILENAME)
+}
+
+async function logCsv(data) {
+  const line = csvLine.encode(data)
+  await fs.appendFile(CSV_FILENAME, `${line}\n`)
+}
+
+!async function() {
+  try {
+    await cleanResultCsv()
+    await showSystemInfo()
+
+    const pkgRoots = await globby(['./packages/*'])
+    const envs     = Object.keys(ENVS)
+
+    await pEachSeries(pkgRoots, async pkgRoot => {
+      await pEachSeries(envs, async env => {
+        const basename = path.basename(pkgRoot)     // owner_repo
+        const repo     = basename.replace('_', '/') // owner/repo
+        log(`Install for ${repo} by ${env}`)
+
+        // clean all cache
+        await Promise.all([
+          cleanLocalCache(pkgRoot),
+          cleanGlobalCache(env),
+        ])
+
+        // install
+        const end  = timeSpan()
+        await installByEnv(env, pkgRoot)
+        const span = end() // ms
+
+        // logging
+        const result = [ repo, env, span ]
+        log('Result:', JSON.stringify(result))
+        await logCsv(result)
+      })
+    })
+
+    console.log('Successful')
+  } catch (e) {
+    console.error('Error', e)
+  }
+}()
